0ce2b7572cf5853977a5c45550e6fc2e1c68ece9
Mikilll <wasniewskimikolaj@gmail.com>
2021-07-18 00:10:24 +0200

    Create basic layout

---
+++ src/layout/Navbar.vue
@@ -0,0 +1,92 @@
+<template>
+  <nav class="nav">
+    <div class="nav-logo">
+      <img
+        :src="require('@/assets/calendar-text-outline.png')"
+        class="nav-logo-icon"
+      />
+      <div class="nav-logo-title">DS-TASK</div>
+    </div>
+    <div class="nav-items">
+      <div class="nav-item">Submit</div>
+      <div class="divider" />
+      <div class="nav-item">Profile</div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.nav {
+  height: 64px;
+  background-color: #1f2937;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  padding: 0 64px;
+}
+
+@media (max-width: 1263px) {
+  .nav {
+    padding: 0 16px;
+  }
+}
+
+.nav-logo {
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+}
+
+.divider {
+  margin-left: 32px;
+  margin-right: 32px;
+  width: 1px;
+  height: 35px;
+  background-color: white;
+
+  @media (max-width: 480px) {
+    margin-left: 16px;
+    margin-right: 16px;
+  }
+}
+
+.nav-logo-title {
+  margin-left: 5px;
+  font-size: 36px;
+  font-family: "Courier New", Courier, monospace;
+  line-height: 0.2;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+
+  @media (max-width: 800px) {
+    display: none;
+  }
+}
+
+.nav-items {
+  position: absolute;
+  display: flex;
+}
+
+.nav-item {
+  font-size: 18px;
+  color: #bec3cb;
+  padding: 8px 12px;
+  border-radius: 6px;
+  &:hover {
+    background-color: rgba(55, 65, 81);
+    cursor: pointer;
+  }
+  &--active {
+    color: #ffffff;
+  }
+}
+</style>
---
+++ src/views/FormView.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="root">
+    <form @submit.prevent="submitForm">
+      <div class="avatar-section">
+        <Avatar class="avatar" :avatar="avatar" size="xxlarge" />
+        <Button appearance="link" @click="changeAvatar">
+          <template v-slot:icon-before>
+            <EditorEditIcon />
+          </template>
+          Upload new image
+        </Button>
+      </div>
+      <input ref="fileInput" type="file" class="d-none" @change="changeFile" />
+      <div class="form">
+        <FieldGroup label="First name" required>
+          <Input v-model="firstName" should-fit-container />
+        </FieldGroup>
+        <FieldGroup label="Last name" required>
+          <Input
+            v-model="lastName"
+            type="number"
+            min="1"
+            max="3"
+            allowed-values="[^0-9\.]"
+            should-fit-container
+          />
+        </FieldGroup>
+        <FieldGroup label="Email" required>
+          <Input
+            v-model="email"
+            type="number"
+            min="1"
+            max="3"
+            allowed-values="[^0-9\.]"
+            should-fit-container
+          />
+        </FieldGroup>
+        <FieldGroup label="Phone" required>
+          <Input
+            v-model="phone"
+            type="tel"
+            allowed-values="[^0-9+]"
+            should-fit-container
+          />
+        </FieldGroup>
+        <FieldGroup label="Birthday" required>
+          <DatePicker v-model="dateOfBirth" :disabled-range="noFutureRange" />
+        </FieldGroup>
+      </div>
+      <FieldGroup label="About" required>
+        <TextArea rows="5" v-model="aboutInfo" />
+      </FieldGroup>
+      <Button class="submitButton" type="submit" appearance="primary">
+        Submit form
+      </Button>
+    </form>
+  </div>
+</template>
+
+<script>
+import {
+  Input,
+  FieldGroup,
+  Avatar,
+  TextArea,
+  Button,
+  DatePicker,
+  EditorEditIcon,
+} from "@spartez/vue-atlaskit";
+
+export default {
+  name: "InputStory",
+  components: {
+    FieldGroup,
+    Input,
+    Avatar,
+    TextArea,
+    Button,
+    DatePicker,
+    EditorEditIcon,
+  },
+  data() {
+    return {
+      avatar: null,
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      dateOfBirth: "",
+      aboutInfo: "",
+      noFutureRange: {
+        from: new Date(),
+      },
+    };
+  },
+  methods: {
+    changeAvatar() {
+      this.$refs.fileInput.click();
+    },
+    changeFile(event) {
+      const file = event.target.files[0];
+      this.avatar = URL.createObjectURL(file);
+    },
+    submitForm() {
+      console.log("lalala");
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.root {
+  margin: 0 auto;
+  padding: 16px 16px 0 16px;
+
+  @media (min-width: 960px) {
+    width: 75%;
+  }
+
+  @media (min-width: 1264px) {
+    width: 50%;
+  }
+}
+
+.avatar-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.avatar {
+  margin-right: 10px;
+}
+
+.form {
+  @media (min-width: 600px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 0 20px;
+  }
+}
+
+.submitButton {
+  margin-top: 20px;
+}
+.d-none {
+  display: none;
+}
+</style>
